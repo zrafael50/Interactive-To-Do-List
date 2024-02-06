7e18ce23e7a5e3c8ff603fec16d2a489c2401b21
Rafael Costa da Silva <contatorafael50@gmail.com>
2024-02-06 17:16:33 -0300

    adicionado "add buton" funcional

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let tarefas = [];
 
-const listItem = document.querySelectorAll(".listItem");
+const lista = document.querySelector("#lista");
+let listaItem = document.querySelectorAll(".listaItem");
 const addButton = document.querySelector("button");
+
+function addField(){
+    const newItem = listaItem[listaItem.length-1].cloneNode(true);
+    lista.appendChild(newItem);   
+    listaItem = document.querySelectorAll(".listaItem");
+
+}
 
 addButton.addEventListener("click", addField);
